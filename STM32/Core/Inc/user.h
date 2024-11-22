aa130f10cbf5065faf996b913a21fe1eccedef16
Fajar Nugraha Purwanto <106831485+fajarnugrahap@users.noreply.github.com>
2024-11-23 01:59:39 +0800

    Add files via upload

---
+++ STM32/Core/Inc/user.h
@@ -0,0 +1,27 @@
+/*
+ * user.h
+ *
+ *  Created on: Jul 12, 2024
+ *      Author: USER
+ */
+
+#ifndef INC_USER_H_
+#define INC_USER_H_
+
+void remote_mode1(void);
+
+void set_speeds(float x_speed,float y_speed,float w_speed);
+
+void set_target (int max_speed, int x_target, int y_target, int theta_target);
+
+void Mleft(int rpm1,int dir1);
+
+void Mback(int rpm3,int dir3);
+
+void Mright(int rpm2,int dir2);
+
+void inverse_odometry (float xpos, float ypos, float thetapos);
+
+void set_speeds2(float x_speed,float y_speed,float w_speed);
+
+#endif /* INC_USER_H_ */
